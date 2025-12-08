773729b5cc41f4e0bf4791ef5e419751e984a750
SakharovIvan <i731901@gmail.com>
2025-12-08 22:02:45 +0300

    added submodule auth serv

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  ClientProxyFactory,
+  ClientsModule,
+  Transport,
+} from '@nestjs/microservices';
 
 @Module({
-  imports: [AuthModule],
+
+  imports: [
+        ConfigModule.forRoot(),
+        AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+        {
+      provide: 'AUTH_SERVICE',
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        ClientProxyFactory.create({
+          transport: Transport.TCP,
+          options: {
+            host: configService.get('AUTH_SERVICE_HOST'),
+            port: configService.get('AUTH_SERVICE_PORT'),
+          },
+        }),
+    },
+    AppService],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 @Module({
+  imports: [
+        ConfigModule.forRoot(),
+
+    ClientsModule.register([
+      {
+        name: 'AUTH_SERVICE',
+        transport: Transport.GRPC,
+        options: {
+          package: 'user',
+          url: process.env.AUTH_SERVICE_SLAVE_HOST + ':' + process.env.AUTH_SERVICE_SLAVE_PORT,
+          loader: { keepCase: true },
+          protoPath: join(__dirname, '../../../lib/B2B_types/proto/user.proto'),
+        },
+      },
+    ]),
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
