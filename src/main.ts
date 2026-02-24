d3eeb6e108c2cb746b7dbbfd49e4d401b884ef0e
SakharovIvan <i731901@gmail.com>
2026-02-24 19:36:46 +0300

    cors added

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    origin: ['http://127.0.0.1:5173'], // разрешенные домены (* — любые)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // разрешаем передачу cookies
+    allowedHeaders: 'Content-Type, Authorization', // разрешаемые заголовки
+    exposedHeaders: 'Authorization', // показываемые клиенту заголовки
+    preflightContinue: false,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
